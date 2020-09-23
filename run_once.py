27696f7eeee962768ad85bede6c42a42bc5921c2
Mateusz Klimek <matkk11@gmail.com>
2020-09-24 00:58:35 +0200

    Add arg parser

--- run_once.py
+++ run_once.py
@@ -1,5 +1,20 @@
 from tasks import send_livecoin_to_graphite, send_stocks_to_graphite
+import argparse
 
 if __name__ == "__main__":
-    send_livecoin_to_graphite()
-    send_stocks_to_graphite()
+    parser = argparse.ArgumentParser(
+        description='Run manual ingestion of metrics (crypto, stocks or both) to graphite'
+    )
+
+    parser.add_argument('--stocks', action='store_true', help='Run ingestion of stocks data')
+    parser.add_argument('--crypto', action='store_true', help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+
+    if not any((args.crypto, args.stocks)):
+        print ("Specify at least one of --stocks --crypto")
+
+    if args.crypto:
+        send_livecoin_to_graphite()
+    if args.stocks:
+        send_stocks_to_graphite()

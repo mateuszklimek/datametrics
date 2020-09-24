09c4068ab4a0eda1d1514eac5347a0a2fea670c9
Mateusz Klimek <matkk11@gmail.com>
2020-09-24 19:09:00 +0200

    update tests

--- tests.py
+++ tests.py
@@ -8,27 +8,43 @@ def has_all_metrics(el):
     return all(metric in el for metric in ['name', 'value', 'interval', 'time'])
 
 
+def get_json_from_file(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+    
+    return data
+
+
 def test_parsing_livecoin():
 
-    with open('tests_data/sample_livecoin.json') as f:
-        data = json.load(f)
+    data = get_json_from_file('tests_data/sample_livecoin.json')
     
     metrics = transform.livecoin_to_metrics(data)
 
     assert len(metrics) == len(CRYPTO_TO_TRACK) * len(CRYPTO_EXPORTED_METRICS)
     assert all(has_all_metrics(el) for el in metrics)
+
+    metric_names = [m['name'] for m in metrics]
+    some_expected_symbols = ['crypto.BTC/USD-last', 'crypto.BTC/USD-low', 'crypto.ETH/USD-last']
+
+    assert all(symbol in metric_names for symbol in some_expected_symbols)
+
     print (metrics)
         
 
 def test_parsing_iex():
 
-    with open('tests_data/sample_iex.json') as f:
-        data = json.load(f)
+    data = get_json_from_file('tests_data/sample_iex.json')
 
     metrics = transform.iex_to_metrics(data)
 
     assert len(metrics) == len(STOCKS_TO_TRACK) * len(STOCKS_EXPORTED_METRICS)
     assert all(has_all_metrics(el) for el in metrics)
+
+    metric_names = [m['name'] for m in metrics]
+    some_expected_symbols = ['stocks.WORK-latestPrice', 'stocks.NFLX-latestPrice', 'stocks.AMZN-latestPrice']
+
+    assert all(symbol in metric_names for symbol in some_expected_symbols)
     print (metrics)
 
 
